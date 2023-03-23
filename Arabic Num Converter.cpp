5c460a73998b55bd1f0e61fad5ade1fef5c2d14a
Charles <126676855+Mr0bsidian@users.noreply.github.com>
2023-03-23 08:19:52 +0100

    Add files via upload

---
+++ Arabic Num Converter.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+
+	std::string toRoman(int number) {
+	const int values[] = { 1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1 };
+	const std::string symbols[] = { "M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I" };
+	std::string result = "";
+	for (int i = 0; i < 13; i++) {
+		while (number >= values[i]) {
+			number -= values[i];
+			result += symbols[i];
+		}
+	}
+	return result;
+}
+
+int main() {
+	int number;
+	std::cout << "Gib eine arabische Nummer ein: ";
+	std::cin >> number;
+	std::cout << "Die roemische Nummer ist: " << toRoman(number) << std::endl;
+	return 0;
+}
